65e759eb866f2d51221347edf1290f5509673d81
dum_jet <d.vasileva2@g.nsu.ru>
2025-09-15 03:09:20 +0900

    lab2 initial (without stack depth)

--- lab02/src/calculate.ts
+++ lab02/src/calculate.ts
@@ -1,5 +1,19 @@
 import { ReversePolishNotationActionDict} from "./rpn.ohm-bundle";
 
 export const rpnCalc = {
-    // write rules here
+    RpnExpr_plus(x, y, _) {
+        return x.calculate() + y.calculate();
+    },
+
+    RpnExpr_times(x, y, _) {
+        return x.calculate() * y.calculate();
+    },
+
+    RpnExpr(e) {
+        return e.calculate();
+    },
+
+    number(_) {
+        return parseInt(this.sourceString);
+    }
 } satisfies ReversePolishNotationActionDict<number>;
--- lab02/src/index.ts
+++ lab02/src/index.ts
@@ -1,16 +1,28 @@
+import { MatchResult } from "ohm-js";
 import grammar from "./rpn.ohm-bundle";
 import { rpnSemantics } from "./semantics";
 
 export function evaluate(source: string): number
 { 
-    throw "Not implemented"
+    return rpnSemantics(parse(source)).calculate();
 }
+
 export function maxStackDepth(source: string): number
 { 
     throw "Not implemented";
 }
 
-export class SyntaxError extends Error
+
+export class SyntaxError extends Error {}
+
+function parse(content: string): MatchResult
 {
+    const match = grammar.match(content);
+    if (match.failed())  {
+        throw new SyntaxError(match.message);
+    }
+    return match;
 }
+
+
 
