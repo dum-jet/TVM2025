bac6f8e2769ec8253a20298ddfc8391655bc2f31
d.vasileva2 <d.vasileva2@g.nsu.ru>
2025-10-27 05:40:02 +0900

    first working version

--- lab05/src/emitHelper.ts
+++ lab05/src/emitHelper.ts
@@ -9,18 +9,18 @@ export async function buildOneFunctionModule<R=number>(name: string, argCount: n
             c.func_type(Array(argCount).fill(c.i32), c.i32), // type index = 0
         ]),
     
-    c.function_section([
-        c.varuint32(0), // function index = 0, uses type index 0
-    ]),
-    
-    c.export_section([
-        c.export_entry(c.str_ascii(name), c.external_kind.function, c.varuint32(0)),
-    ]),
+        c.function_section([
+            c.varuint32(0), // function index = 0, uses type index 0
+        ]),
+        
+        c.export_section([
+            c.export_entry(c.str_ascii(name), c.external_kind.function, c.varuint32(0)),
+        ]),
 
-    c.code_section([
-        // body of function at index 0:
-        c.function_body([ /* no additional local variables */ ], body)]
-    )]
+        c.code_section([
+            // body of function at index 0:
+            c.function_body([ /* no additional local variables */ ], body)]
+        )]
     );
     const emitter = new BufferedEmitter(new ArrayBuffer(mod.z));
     mod.emit(emitter);
